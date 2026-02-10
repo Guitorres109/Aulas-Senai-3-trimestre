f3e3d75ee66430214867f77ef449039c3933e5ff
Guilherme <torres.guib2009@gmail.com>
2026-02-10 14:36:53 -0300

    primeiro commit

---
+++ readme.md
@@ -0,0 +1,50 @@
+# Principais tipos de dados Javascript
+
+### String: (Texto):
+
+- Sempre é escrita entre aspas (" " ou ' ')
+
+### Number: (numero):
+
+- No JavaScript, número podem ser inteiros ou decimais
+
+### Boolean: (verdadeiro ou falso):
+
+- Um boolean pode ter apenas dois valores: true e false
+
+### Object: (objeto):
+
+- Um objeto agrupa várias informações em uma só variavel
+
+### Array: (listas ou valores):
+
+### Null: (nulo):
+
+### Undefined: (Não definido):
+
+### Anotações:
+
+- Usar 'typeof' para descobrir tipo de um valor ou variavel
+- Template String: ```console.log(`Eu moro em ${cidade}.`)```
+
+### Camel Case:
+- Primeira palavra comaça com letras minusculas
+- Palavras seguintes comaçam com letras maiuscula
+- ```let idadeUsuario = undefined```
+
+### Pascal Case:
+- Todas as palavras começam com a letra maiuscula
+- Usada por classes, construtores e componentes
+- ```let IdadeUsuario = undefined```
+
+### Snake Case:
+
+- Palavras Separadas por underscore "_"
+- Tudo Minusculo
+- Muito comum em python, banco de dados e APIs
+- ```let nome_completo_casado```
+
+### Kebab Case:
+- Palavras separadas por hífen "-"
+- Não funciona como nomes de variaveis em JavaScript
+- ```<div class="menu-principal"></div>```
---
+++ script.js
@@ -0,0 +1,75 @@
+//String
+//------------------------------------------------------------------------------------------------------------------------------------
+
+let meuNome = "Guilherme"
+let meuSobrenome = "Torres"
+
+console.log(`Meu nome completo é ${meuNome} ${meuSobrenome}`)
+
+let escola = "SESI Guarulhos"
+console.log("Minha esola é "+ escola)
+
+let string1 = "Oi"
+let string2 = "Bom dia!"
+
+console.log(string1 + ", " + string2)
+
+console.log("Bem vindo a nosso sistema "+ meuNome+" "+ meuSobrenome)
+
+console.log(meuNome.length)
+
+let maiusculas = meuNome.toUpperCase
+let minusculas = meuNome.toLowerCase
+
+console.log(maiusculas)
+console.log(minusculas)
+
+console.log(`Olá meu nome é ${meuNome} ${meuSobrenome} e moro na cidade de Guarulhos`)
+
+//Numeros
+//------------------------------------------------------------------------------------------------------------------------------------
+
+let n1 = 42
+let n2 = 58
+let soma = n1+n2
+
+console.log(`A soma de ${n1} com ${n2} é ${soma}`)
+
+let sub = n1-n2
+
+console.log(`A subtração de ${n1} com ${n2} é ${sub}`)
+
+let mult = n1*n2
+
+console.log(`A multiplicação de ${n1} com ${n2} é ${mult}`)
+
+let div = n1/n2
+
+console.log(`A divisão de ${n1} com ${n2} é ${div}`)
+
+//Boolean
+//------------------------------------------------------------------------------------------------------------------------------------
+
+let ativo = true
+
+if (n1>10){
+    ativo = true
+}
+else {
+    ativo = false
+}
+
+idade = 16
+maiorIdade = null
+
+if (idade<18){
+    maiorIdade = false
+}
+else{
+    maiorIdade = true
+}
+
+
+
+
+
